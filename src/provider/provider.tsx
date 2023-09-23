cd9f1be489e670df70f470a7fa03196079914170
William Cairns <cairnswm@gmail.com>
2023-09-23 16:34:43 +0200

    Add Config to Provider

--- src/provider/provider.tsx
+++ src/provider/provider.tsx
@@ -1,27 +1,54 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
+type KeyValue = {
+  key: string;
+  value: string;
+};
 type TenantType = {
   tenant: string
+  config: KeyValue[]
 }
 type TenantProviderType = {
   children: React.ReactNode
   application: string
+  config: string
 }
 
 // create context
-const TenantContext = createContext<TenantType>({ tenant: "" });
+const TenantContext = createContext<TenantType>({ tenant: "", config: [] });
 
 const TenantProvider = ( props: TenantProviderType ) => {
-  const { children } = props;
+  const { children, config: configFile } = props;
 
   if (!props.application) {
     throw new Error("TenantProvider: application prop is required");
   }
 
+  useEffect(() => {
+    if (!configFile) {
+      console.warn("No config file provided");
+    }
+  }, [configFile]);
+
   const [tenant, ] = useState(props.application);
+  const [config, setConfig] = useState<KeyValue[]>([]);
+  const hostname = window.location.hostname;
+
+  const loadConfig = () => {
+    setConfig([])
+  }
+
+  if (!config) {
+    // debugger;
+    loadConfig();
+  }
+
+  useEffect(() => {
+    loadConfig();
+  }, [hostname, props]);  
 
   return (
-    <TenantContext.Provider value={{tenant}}>
+    <TenantContext.Provider value={{tenant, config}}>
       {children}
     </TenantContext.Provider>
   );
