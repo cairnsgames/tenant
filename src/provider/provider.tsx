f57bd58fa2ecabdf2e5a7e4374a254c0a036c5b8
William Cairns <cairnswm@gmail.com>
2023-09-26 09:22:27 +0200

    Remove all config functionality

--- src/provider/provider.tsx
+++ src/provider/provider.tsx
@@ -1,54 +1,29 @@
 import React, { createContext, useState, useEffect } from "react";
 
-type configValuesType = {
-  env: {[key: string]: any[]}
 }
 type TenantType = {
   tenant: string
-  config: configValuesType
 }
 type TenantProviderType = {
   children: React.ReactNode
   application: string
-  config: string
 }
 
 // create context
 const TenantContext = createContext<TenantType>({ tenant: "", config: {env: {}} });
 
 const TenantProvider = ( props: TenantProviderType ) => {
-  const { children, config } = props;
+  const { children } = props;
 
   if (!props.application) {
     throw new Error("TenantProvider: application prop is required");
   }
 
-  useEffect(() => {
-    if (!config) {
-      console.warn("No config file provided");
-    }
-  }, [config]);
 
   const [tenant, ] = useState(props.application);
-  const [configValues, setConfigValues] = useState<configValuesType>({env: {}});
-  const hostname = window.location.hostname;
-
-  const loadConfig = () => {
-    // add code here to load config from env (or process.enf if we get webpack working)
-    setConfigValues({ env : {}})
-  }
-
-  if (!config) {
-    // debugger;
-    loadConfig();
-  }
-
-  useEffect(() => {
-    loadConfig();
-  }, [hostname, props]);  
 
   return (
-    <TenantContext.Provider value={{tenant, config: configValues}}>
+    <TenantContext.Provider value={{tenant}}>
       {children}
     </TenantContext.Provider>
   );
